f510677877b3194db88efa6139e024165a6cbaa8
zcittadin <z_cittadin@hotmail.com>
2017-09-19 16:59:27 -0300

    Adição de ícone de aplicação

--- src/main/java/com/servicos/estatica/resicolor/app/Main.java
+++ src/main/java/com/servicos/estatica/resicolor/app/Main.java
@@ -1,12 +1,12 @@
 package com.servicos.estatica.resicolor.app;
 
-
 import com.servicos.estatica.resicolor.util.HibernateUtil;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,8 +16,9 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		stage.setScene(new Scene(root));
 		stage.setTitle("Resicolor - Sistema de consultas de processo");
-		// stage.setMaximized(true);
+		stage.setMaximized(false);
 		stage.setResizable(false);
+		stage.getIcons().add(new Image("/com/servicos/estatica/resicolor/style/paintcan.png"));
 		stage.show();
 	}
 
